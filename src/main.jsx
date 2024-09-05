a02a72a541d7698ca9e59d7712a9923f9862f314
danielite123 <danielite100@gmail.com>
2024-09-05 15:05:19 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,26 @@ root.render(
   <HelmetProvider>
     <BrowserRouter>
       <Suspense>
-        <ToastContainer />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          limit={1} // Limits the number of toasts shown at once
+          style={{
+            width: '200px', // Set a smaller width for the toast container
+            padding: '8px', // Reduce padding for a more compact look
+          }}
+          toastStyle={{
+            fontSize: '14px', // Reduce the font size for smaller text
+            padding: '10px', // Reduce the padding inside each toast
+          }}
+        />
         <App />
       </Suspense>
     </BrowserRouter>
